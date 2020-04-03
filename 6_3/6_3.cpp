27c627aac4b7389d58c2ee9cb370df3c6c4cd407
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-04-03 14:37:39 +0800

    6_3

--- 6_3/6_3.cpp
+++ 6_3/6_3.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv.hpp>
+using namespace cv;
 
 int main()
 {
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("D:\\lena.jpg", 1);
+	if(srcMat.empty()) return -1;
+
+	const cv::Point2f src_pt[] = {
+		cv::Point2f(200, 200),
+		cv::Point2f(250, 200),
+		cv::Point2f(200, 100) };
+	const cv::Point2f dst_pt[] = {
+		cv::Point2f(300, 100),
+		cv::Point2f(300, 50),
+		cv::Point2f(200, 100) };
+
+	const cv::Mat affine_matrix = cv::getAffineTransform(src_pt, dst_pt);
+
+	cv::warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
+	
+	cv::imshow("src", srcMat);
+	cv::imshow("dst", dstMat);
+	cv::waitKey(0);
     return 0;
 }
 
